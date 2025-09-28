b1ee82755d9dee06dc62d3da20e61decc2ab1e8c
sumit-vishwakarma9626 <sumitvishwakarma8125@gmail.com>
2025-09-28 14:47:18 +0530

    learning python

---
+++ task_3.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number: "))
+
+if a %2 == 0:
+    print(f"{a} is even number")
+
+else:
+    print(f"{a} is odd number")
---
+++ task_4.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the number: "))
+i = 1
+sum = 0
+while i<=a:
+    sum += i
+    i+=1
+
+print(sum)
