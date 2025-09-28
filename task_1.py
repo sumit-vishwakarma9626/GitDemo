ddc5b21ee6b01808ccfcf27958d4a2ca392d6f04
sumit-vishwakarma9626 <sumitvishwakarma8125@gmail.com>
2025-09-28 13:36:59 +0530

    this is my first task

---
+++ task_1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"Addition : {a+b}")
+print(f"subtraction: {a-b}")
+print(f"multiplication: {a*b}")
+print(f"division : {a/b}")
